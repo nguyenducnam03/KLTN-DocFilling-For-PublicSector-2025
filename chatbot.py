86c31ed0e554809a3d99a6aef453f347edc192b5
Duc Nam Nguyen <nguyenducnamk3@gmail.com>
2024-04-13 18:58:22 +0700

    feat: add branch test, represented again OOP format, MyClasses, constant_value

--- chatbot.py
+++ chatbot.py
@@ -1,40 +1,10 @@
-from langchain_community.llms import HuggingFaceEndpoint
-from langchain_google_genai import GoogleGenerativeAI, HarmBlockThreshold, HarmCategory
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableConfig, RunnableLambda
-from langchain.prompts import ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory
-# Database
-from langchain_community.utilities import SQLDatabase
-# Chainlit
 import chainlit as cl
-from chainlit.input_widget import Select, Switch, Slider
-from chainlit.playground.config import add_llm_provider
-from chainlit.playground.providers.langchain import LangchainGenericProvider
-from chainlit.types import ThreadDict
-# Import file khác
-from operator import itemgetter
-from uniform_text import *
-from txt_filling import *
-from blank_to_tagname import *
-from extract_context import *
-from database import *
-import os.path
-#Genai
-import google.generativeai as genai
+from chainlit.input_widget import Switch, Slider
 
-# Prompt
-blank_to_tagname_prompt, tag_names, translations = blank_to_tagname_prompt()
-extract_info_prompt = extract_info_prompt()
-prompt_miss_info =  Identify_missing_info()
+#Import from another files
+import MyClasses
+import constant_value as CONST
 
-# check exist database
-if not os.path.isfile("data.db"):
-    keys = "ID" + tag_names
-    create_database(keys)
-
-
-# ----------------------- On start -----------------------------
 @cl.on_chat_start
 async def on_chat_start():
     # Chat setting
@@ -47,44 +17,15 @@ async def on_chat_start():
         Switch(id="New", label="New", initial=True),
         ]
     ).send()
-    # Set up the model
-    genai.configure(api_key="AIzaSyBRWVbQgcq1F5-1jXqIGC30MQ1ASMSaM50")
-    generation_config = {
-    "temperature": 0,
-    "top_p": 1,
-    "top_k": 1,
-    "max_output_tokens": 6144,
-    }
-    safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-    }
-    ]
-    # ................................ LLM ......................................
-    llm = genai.GenerativeModel(model_name="gemini-pro",
-                              generation_config=generation_config,
-                              safety_settings=safety_settings)
-    # -............................ Ảnh bìa ............................
+
+    # ======================Ảnh bìa======================
     image = cl.Image(path="./fill_form.jpg", name="image1", display="inline")
     await cl.Message(
         content="Tôi là chatbot có nhiệm vụ chính là điền thông tin vào document.",
         elements=[image],
     ).send()  
 
-    # ------------------Đưa form vào chatbot bất cứ khi nào cũng được------------------
+    # ======================Insert file need to fullfill======================
     files = None
     while files == None:
         files = await cl.AskFileMessage(
@@ -94,221 +35,95 @@ async def on_chat_start():
     with open(text_file.path, "r", encoding="utf-8") as f:
         text = f.read()  
 
-    #Notify upload text successfully
-    await cl.Message(content=f"`{text_file.name}` uploaded, it contains {len(text)} characters!").send() # Thông báo cho người dùng biết đã up file thành công
-    # await cl.Message(content=f"Content \n {text}").send()
+    # print("text \n:",text)
+    await cl.Message(content=f"Content:").send()
+    await cl.Message(content=text).send()
 
-    #------------------Text processing------------------
-    text, count_blank = generate_uniform(text)
-    await cl.Message(content=f"Processed Text : \n {text}").send()
-    list_tag_names = blank_to_tagname(llm, text, tag_names)
-    # cl.user_session.set('list_tag_names', list_tag_names)
-    # await cl.Message(content=list_tag_names).send()
-    # list_keys = get_list_keys(list_tag_names, translations)
-    # print("LIST_KEYS: ", list_keys)
-    # cl.user_session.set('list_keys', list_keys)
-    # # --------------------------- Database --------------------------
-    # count_id = count_rows()
-    # if count_id == 0:
-    #     await cl.Message(content = "Chưa có thông tin trong database. Vui lòng đưa context vào.").send()
-    #     value = "1"
-    # else:
-    #     list_name_actions = [cl.Action(name = "New", value = str(count_id + 1), label = "0: New")]
-    #     for i in range(count_id):
-    #         name = get_value(i+1,"Full_Name")
-    #         info = f"{i+1}: {name}"
-    #         list_name_actions.append(cl.Action(name = f"Row {i+1}", value = str(i + 1), label = info ))
-    #     res = await cl.AskActionMessage(
-    #         content=f"Chọn lựa chọn phù hợp.",
-    #         actions= list_name_actions
-    #     ).send()
-    #     value = res.get("value")
-    # print(count_id, value)
-    # if value != str(count_id + 1): # Điền form bằng cách lấy thông tin từ database
-    #     list_info, list_miss_keys, list_miss_items = get_values(value, list_tag_names, translations)
-    #     output_form = fill_form(text, list_info, count_blank)
-    #     await cl.Message(content = output_form).send()
-    #     with open('./Output/result.txt','w',encoding='utf-8') as file:
-    #         file.write(output_form)
-    #     print("11111111111111111111111111111111111")
-    #     print("LIST_INFO:", list_info)
-    #     print("LIST_MISS_KEYS:", list_miss_keys)
-    #     print("LIST_MISS_ITEMS:", list_miss_items)   
-    # # ---------------------- Lưu user session ---------------------
-    # cl.user_session.set("value", value)
-    # cl.user_session.set("text",text)
-    # cl.user_session.set("count_blank", count_blank)
-    # cl.user_session.set("count_id", count_id)
+    ## ======================Handling======================
+    llm = MyClasses.LLM_Gemini(CONST.API_KEY)
+    handle_text = MyClasses.Text_Processing()
 
+    blanked_text, count_blank = handle_text.generate_uniform(text)
+    await cl.Message(content=f"Blanked text: \n {blanked_text}").send()
+    blank_to_tagnames = llm.blank_to_tagnames(blanked_text, CONST.tag_names) ##**Important**
 
-# ---------------------------------------- On message --------------------------------------
+    list_tag_names = list(blank_to_tagnames.values())
+    # await cl.Message(content=f"List of tagnames: \n {list_tag_names}").send()
+    list_values = llm.translate_tag_names(list_tag_names, CONST.translations)
+    # await cl.Message(content=f"List of values: \n {list_values}").send()
+
+    ## ======================DATABASE======================
+    '''Pro làm nhen'''
+
+    #Requires users to enter content
+    await cl.Message(
+        content = "Hãy nhập thông tin để tôi giúp bạn điền vào!"
+    ).send()
+
+    # # ---------------------- Save user session ---------------------
+    cl.user_session.set("blank_to_tagnames",blank_to_tagnames)
+    cl.user_session.set("count_blank",count_blank)
+    cl.user_session.set("handle_text",handle_text)
+    cl.user_session.set("llm",llm)
+    cl.user_session.set("blanked_text", blanked_text)
+    cl.user_session.set("list_tag_names", list_tag_names)
+    cl.user_session.set("list_values", list_values)
+
 @cl.on_message
-async def main(message: cl.Message):   
-    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
-    # Code tiếp tục đoạn chat trước đó
-    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
-    # Tag name cần điền từ form:
-    list_tag_names = cl.user_session.get('list_tag_names')
-    # ---------------------- Người dùng chọn việc load thông tin từ database hay điền người mới------------------------------
-    count_id = cl.user_session.get("count_id")
-    value = cl.user_session.get("value")
-    list_keys = cl.user_session.get("list_keys")
-    list_info = []
-    list_miss_items = []
-    list_miss_keys = []
-    list_index = []
-    if value == str(count_id + 1): # Điền form bằng context được nhập vào
-        print("22222222222222222222222222222222222")
-        # ----------------- Extract information ----------------------------------
-        llm = cl.user_session.get('llm')
-        extract_info_chain = (
-            RunnablePassthrough.assign(
-                history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
-            )
-            | extract_info_prompt
-            | llm
-            | StrOutputParser()
-        )
-        print("LIST_KEYS: ", list_keys)
-        Question_temp = """"""
-        for item in list_keys:
-            Question_temp += item + "\n"
-        res = await extract_info_chain.ainvoke({
-                "Abstract": message.content, 
-                "Question": Question_temp
-            })
-        list_info,list_miss_items,list_index = get_listInfo_and_missItem(res, list_info, list_miss_items, list_index)
-        count_miss_items = len(list_miss_items)
-        if count_miss_items:
-            for value_temp in list_miss_items:
-                list_miss_keys.append(find_key_by_value(translations, value_temp))
-        
-        # for i, Question in enumerate(list_keys):
-        #     count_miss_items1 = len(list_miss_items)
-        #     res = await extract_info_chain.ainvoke({
-        #         "Abstract": message.content, 
-        #         "Question": Question
-        #     })
-        #     list_info, list_miss_items = get_listInfo_and_missItem(res, list_info, list_miss_items)
-        #     count_miss_items2 = len(list_miss_items)
-        #     if count_miss_items1 != count_miss_items2:
-        #         list_index.append(i)
-        #         list_miss_keys.append(list_tag_names[i])
-        await cl.Message(content = f"Thông tin phù hợp với từng Blank: {list_info}").send()
-        await cl.Message(content = f"Thông tin còn thiếu: {list_miss_items}").send()
-        print("LIST_INFO:", list_info)
-        print("LIST_MISS_KEYS:", list_miss_keys)
-        print("LIST_MISS_ITEMS:", list_miss_items)
-        # Thêm thông tin vào database
-        data_to_insert = create_tag_info_dict(value, list_tag_names, list_info)
-        print("DATA_TO_INSERT", data_to_insert)
-        insert_value_into_database(data_to_insert)
-
-    # Query user 
-    count =  len(list_miss_items)
-    data_to_update = {"ID": value}
-    while(count):
-        query = f"Thông tin về '{list_miss_items[len(list_miss_items)-count]}' hiện đang thiếu, bạn hãy cung cấp thêm thông tin này."
-        res = await cl.AskUserMessage(content = query, timeout=30).send()
-        if res:
-            await cl.Message(content=f"{list_miss_items[len(list_miss_items)-count]}: {res['output']}").send()
-        list_info[list_index[len(list_miss_items)-count]] = res['output']
-        miss_item_temp = list_miss_keys[len(list_miss_keys)-count].replace("#","")
-        data_to_update[miss_item_temp] = res['output']
-        count -= 1
-    update_value_in_database(value,data_to_update)
-    # Fill Form
-    text = cl.user_session.get("text")
+async def main(message: cl.Message):
+    # cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    # # ---------------------- Take again user session ---------------------
+    blank_to_tagnames = cl.user_session.get("blank_to_tagnames")
     count_blank = cl.user_session.get("count_blank")
-    output_form = fill_form(text, list_info, count_blank)
-    await cl.Message(content = output_form).send()
-    with open('./Output/result.txt','w',encoding='utf-8') as file:
-        file.write(output_form)
-    # Memory
-    memory = cl.user_session.get("memory")
-    memory.chat_memory.add_user_message(message.content)
+    handle_text = cl.user_session.get("handle_text")
+    llm = cl.user_session.get("llm")
+    blanked_text = cl.user_session.get("blanked_text")
+    list_tag_names = cl.user_session.get("list_tag_names")
+    list_values = cl.user_session.get("list_values")
 
-# ------------------------------ Câu lệnh in ra khi user dừng -------------------------------
+    
+    # Get response
+    context = message.content
+
+    #Get infor context by LLM
+    value_keys_to_context_value = llm.extract_content(context, list_values) ##**Important**
+    # print("value_keys_to_context_value: \n",value_keys_to_context_value)
+    # await cl.Message(
+    #     content = f"value_keys_to_context_value: \n {value_keys_to_context_value}"
+    # ).send()  
+
+    ##============================Filled Form============================
+    filled_form = handle_text.fill_form(blanked_text, blank_to_tagnames, value_keys_to_context_value)
+    # print("filled_form: \n",filled_form)
+    # await cl.Message(
+    #     content = f"Filled form: \n {filled_form}"
+    # ).send()
+    
+
+# ------------------------------ Stop section ------------------------------
 @cl.on_stop
 async def on_stop():
     print("Người dùng muốn dừng công việc này!")
 
-# ------------------ Câu lệnh in ra khi người dùng ngắt kết nối ------------------------------
+# ------------------------------ End section ------------------------------
 @cl.on_chat_end
 async def on_chat_end():
     print("Người dùng đã ngắt kết nối!")
 
-
-# --------------------------- Xác thực -------------------------------
-@cl.password_auth_callback
-def auth_callback(username: str, password: str):
-    # Fetch the user matching username from your database
-    # and compare the hashed password with the value stored in the database
-    if (username, password) == ("LHH", "1323"):
-        return cl.User(
-            identifier="LHH", metadata={"role": "admin", "provider": "credentials"}
-        )
-    else:
-        return None
+# --------------------------- Authentication -------------------------------
+# @cl.password_auth_callback
+# def auth_callback(username: str, password: str):
+#     # Fetch the user matching username from your database
+#     # and compare the hashed password with the value stored in the database
+#     if (username, password) == ("LHH", "1323"):
+#         return cl.User(
+#             identifier="LHH", metadata={"role": "admin", "provider": "credentials"}
+#         )
+#     else:
+#         return None
     
-# ---------------------------- Chat profile ---------------------------------
-@cl.set_chat_profiles
-async def chat_profile(current_user: cl.User):
-    if current_user.metadata["role"] != "admin":
-        return None
-
-    return [
-        cl.ChatProfile(
-            name="Gemma-7b-it",
-            markdown_description="Gemma is a family of lightweight, state-of-the-art open models from Google, built from the same research and technology used to create the Gemini models.",
-        ),
-    ]    
-
 # ----------------------------- Chat settings update --------------------------
-@cl.on_settings_update
-async def setup_agent(settings):
-    print("on_settings_update", settings)
+# @cl.on_settings_update
+# async def setup_agent(settings):
+#     print("on_settings_update", settings)
 
-
-# --------------------------- Resume ------------------------------------------
-@cl.on_chat_resume
-async def on_chat_resume(thread: ThreadDict):
-    memory = ConversationBufferMemory(return_messages=True)
-    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
-    for message in root_messages:
-        if message["type"] == "USER_MESSAGE":
-            memory.chat_memory.add_user_message(message["output"])
-        else:
-            memory.chat_memory.add_ai_message(message["output"])
-
-    cl.user_session.set("memory", memory)
-
-    # Setting
-    settings = await cl.ChatSettings(
-        [
-        Slider(id="Temperature",label="Temperature",initial=0.01,min=0,max=1,step=0.02,),
-        Slider(id="Top-k",label = "Top-k", initial = 20, min = 1, max = 100, step = 1),
-        Slider(id="Top-p",label = "Top-p",initial = 0.95, min = 0,max = 1,step = 0.02),
-        Slider(id="mnt", label = "Max new tokens", initial = 4000, min = 0, max = 5000, step = 100),
-        Switch(id="New", label="New", initial=True),
-        ]
-    ).send()
-        #
-    generation_config = {
-        "temperature": settings['Temperature'],
-        "top_p": settings['Top-p'],
-        "top_k": settings['Top-k'],
-        "max_output_tokens": settings['mnt'],
-    }
-    # ................................ LLM ......................................
-    llm = GoogleGenerativeAI(
-        model="gemini-pro",
-        safety_settings={
-        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
-        },
-        generation_config=generation_config,
-        google_api_key="AIzaSyBRWVbQgcq1F5-1jXqIGC30MQ1ASMSaM50")
-    await cl.Message(content=f"Tiếp tục cuộc hội thoại.").send()
